e5cce803d918d37701cd4f21253062be56f69ce4
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-04-02 17:45:35 +0300

    SpawnManagerKısımlarıTamamlandı

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
-
+using System.Collections;
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject[] objectsToSpawn;
+    public Transform spawnPoint;
+    public float minSpawnInterval;
+    public float maxSpawnInterval;
+    public float objectLifetime;
+    public float moveSpeed; // Nesnelerin kayma hýzý
+
+    private void Start()
     {
-        
+        StartCoroutine(SpawnObjects());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnObjects()
     {
-        
+        while (true)
+        {
+            float randomTime = Random.Range(minSpawnInterval, maxSpawnInterval);
+            yield return new WaitForSeconds(randomTime);
+
+            if (objectsToSpawn.Length > 0)
+            {
+                int randomIndex = Random.Range(0, objectsToSpawn.Length);
+                GameObject spawnedObject = Instantiate(objectsToSpawn[randomIndex], spawnPoint.position, Quaternion.identity);
+
+                // Nesneye hareket scriptini ekle ve hýzýný ayarla
+                MoveObject moveScript = spawnedObject.AddComponent<MoveObject>();
+                moveScript.speed = moveSpeed;
+
+                // Belirtilen süre sonra yok et
+                Destroy(spawnedObject, objectLifetime);
+            }
+        }
     }
 }
